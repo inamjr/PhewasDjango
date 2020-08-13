d972df5fc1ac47a0f4eddda6533e9517166fa0f4
inamjr <ikeogu56@gmail.com>
2020-08-13 07:55:18 -0600

    adding serialiser

--- PhewasDjango/PhewasDjango/settings.py
+++ PhewasDjango/PhewasDjango/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',   # <- add this
+    'api_basic', # <- add this
     'main.apps.MainConfig', # <- add this
 ]
 
