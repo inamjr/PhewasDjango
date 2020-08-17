4c4be8c2147b3be81013b231d3f001e7c50b0b06
inamjr <ikeogu56@gmail.com>
2020-08-17 08:03:56 -0600

    db conections

--- PhewasDjango/PhewasDjango/settings.py
+++ PhewasDjango/PhewasDjango/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -75,22 +76,24 @@ WSGI_APPLICATION = 'PhewasDjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'labwasWeb',
-#         'USER': 'anthony',
-#         'PASSWORD': 'password',
-#         'HOST': 'anthonydb.crwkqnreqbx2.us-east-2.rds.amazonaws.com',
-#         'PORT': '3306',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
-# }
+#}
+#DATABASES = dj_database_url.config()
+#DATABASES['default'] = dj_database_url.config(mysql://anthonydb.crwkqnreqbx2.us-east-2.rds.amazonaws.com:3306/labwasWeb2?user=anthony&password=password)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'labwasWeb2',
+        'USER': 'anthony',
+        'PASSWORD': 'password',
+        'HOST': 'anthonydb.crwkqnreqbx2.us-east-2.rds.amazonaws.com',
+        'PORT': '3306',
+    }
+}
 
 
 # Password validation
--- PhewasDjango/main/views.py
+++ PhewasDjango/main/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse , JsonResponse
 from rest_framework.parsers import JSONParser
-from .models import PheWasModel
-from .serializers import PhewasSerializer
+from .models import PheWasModel,PheWasModel_2
+from .serializers import PhewasSerializer,PhewasSerializer2
 
 # Create your views here.
 
@@ -12,8 +12,20 @@ def PheWas_List(request):
         phewas = PheWasModel.objects.all()
         serializer = PhewasSerializer(phewas,many=True)
         return JsonResponse(serializer.data, safe=False)
-        
 
+     
+def PheWas_List_2(request):
+    if request.method =='GET':
+        phewas2 = PheWasModel_2.objects.all()
+        serializer2 = PhewasSerializer2(phewas2,many=True)
+        return JsonResponse(serializer2.data, safe=False)
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer2 = PhewasSerializer2(data=data)
+        if serializer2.is_valid():
+            serializer2.save()
+            return JsonResponse(serializer2.data, status=201)
+        return JsonResponse(serializer2.errors, status=400)
 
 def index(response):
     return HttpResponse("VUMC rocks!!")
